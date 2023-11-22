e4461d8f090042696da8eb02f5e30b59f17a20e2
Replit user <>
2023-11-22 04:47:19 +0000

    Insertion sort formatted

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -1,11 +1,12 @@
+#Insertion Sort Algorithm
 nums = input('Enter nums: ')
 nums = nums.split(' ')
 nums = [int(k) for k in nums]
 for i in range(1, len(nums)):
-    key = nums[i]
-    j = i - 1
-    while j > -1 and nums[j] > key:
-        nums[j + 1] = nums[j]
-        j -= 1
-    nums[j + 1] = key
+  key = nums[i]
+  j = i - 1
+  while j > -1 and nums[j] > key:
+    nums[j + 1] = nums[j]
+    j -= 1
+  nums[j + 1] = key
 print(nums)
