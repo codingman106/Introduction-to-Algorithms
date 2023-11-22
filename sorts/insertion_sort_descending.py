f75df893a4a180e424f6a1d0eca0fc71dff26352
Replit user <>
2023-11-22 04:38:09 +0000

    Insertion sort, but in decreasing order

---
+++ sorts/insertion_sort_descending.py
@@ -0,0 +1,11 @@
+nums = input('Enter nums: ')
+nums = nums.split(' ')
+nums = [int(k) for k in nums]
+for i in range(1, len(nums)):
+    key = nums[i]
+    j = i - 1
+    while j > -1 and nums[j] < key:
+        nums[j + 1] = nums[j]
+        j -= 1
+    nums[j + 1] = key
+print(nums)
